cb04f7f2c81e3cf3d01260f750d53817c9dd5780
userTAR <tomas.a.rivero.01@gmail.com>
2020-10-27 21:30:04 -0300

    Excepciones .fV1

---
+++ TP3/Rivero.Tomas.2A.TP3/Excepciones/ArchivosException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class ArchivosException : Exception
+    {
+        public ArchivosException() : base("Archivo no válido")
+        {
+
+        }
+        public ArchivosException(Exception innerException) : base("Archivo no válido",innerException)
+        {
+
+        }
+    }
+}
---
+++ TP3/Rivero.Tomas.2A.TP3/Excepciones/DniInvalidoException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class DniInvalidoException : Exception
+    {
+        public DniInvalidoException() : base("El número de DNI no es válido")
+        {
+
+        }
+        public DniInvalidoException(Exception innerException) : base("El número de DNI no es válido", innerException)
+        {
+
+        }
+        public DniInvalidoException(string mensaje) : base(mensaje)
+        {
+
+        }
+        public DniInvalidoException(string mensaje, Exception innerException) : base(mensaje, innerException)
+        {
+
+        }
+    }
+}
