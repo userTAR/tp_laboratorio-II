9b5a983022fb3d920833f1ae94bfd6df5a93f8a7
userTAR <tomas.a.rivero.01@gmail.com>
2020-11-28 18:19:52 -0300

    TestUnitario fV.1

---
+++ TP4/Rivero.TomasAgustin.2A.TP4/TestUnitario/UnitTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Entidades;
+using Excepciones;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestAgregarADeposito()
+        {
+            Deposito<Helicoptero> depHeli = new Deposito<Helicoptero>();
+            Helicoptero helicoptero = new Helicoptero("Robinson", "R60", 80, 60000, 1, 3, 29000);
+            depHeli += helicoptero;
+
+            Assert.IsNotNull(depHeli.Lista);
+        }
+        [TestMethod]
+        public void TestExcepcionTechoOperativo()
+        {
+            bool check = false;
+            try
+            {
+                Deposito<Helicoptero> depHeli = new Deposito<Helicoptero>();
+                Helicoptero helicoptero = new Helicoptero("Robinson", "R60", 80, 60000, 1, 3,58000);
+                depHeli += helicoptero;
+            }
+            catch(TechoOpException e)
+            {
+                check = true;
+            }
+            Assert.IsTrue(check);
+        }
+        [TestMethod]
+        public void TestVelocidadExcepcion()
+        {
+
+            bool check = false;
+            try
+            {
+                Helicoptero helicoptero = new Helicoptero("Robinson", "R60",0, 60000, 1, 3, 58000);
+            }
+            catch (VelocidadException e)
+            {
+                check = true;
+            }
+            Assert.IsTrue(check);
+        }
+    }
+}
